97e61bdaccc2bcd264321eff185a7b983898819f
Ben Kaminski <ben@benkaminski.com>
2021-07-06 13:37:27 -0400

    initial WP commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+<?php get_template_part( 'parts/page', 'header-tracking' ); ?>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="alternate" hreflang="x-default" href="<?php echo get_home_url(); ?>"/>
+<link rel="alternate" hreflang="en-us" href="<?php echo get_home_url(); ?>"/>
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div class="header-menu">
+
+</div>
